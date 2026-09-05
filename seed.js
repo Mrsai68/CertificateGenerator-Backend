8bf0681cb1e4acb38d40cc66020e9cf518af3401
Admin <sairaj0608@gmail.com>
2026-09-05 11:58:39 +0530

    Server

--- seed.js
+++ seed.js
@@ -22,44 +22,6 @@ const seedData = async () => {
       await adminUser.save();
     }
 
-    const existingHod = await User.findOne({ username: 'hod_computer' });
-    if (!existingHod) {
-      const hodUser = new User({
-        username: 'hod_computer',
-        email: 'hod.computer@gpmiraj.ac.in',
-        password: 'Gpm@hod_comp',
-        role: 'ROLE_HOD',
-        department: 'Computer Engineering',
-        isActive: true
-      });
-      await hodUser.save();
-    }
-
-    const existingStudent = await User.findOne({ username: 'student1' });
-    if (!existingStudent) {
-      const studentUser = new User({
-        username: 'student1',
-        email: 'student1@gmail.com',
-        password: 'Std1',
-        role: 'ROLE_STUDENT',
-        department: 'Computer Engineering',
-        isActive: true
-      });
-      const savedStudent = await studentUser.save();
-
-      const profile = new StudentProfile({
-        user: savedStudent._id,
-        enrollmentNo: '2105120001',
-        fullName: 'Rahul Suresh Patil',
-        department: 'Computer Engineering',
-        yearOfStudy: 'Third Year / Final Year',
-        academicYear: '2025-2026',
-        gender: 'Male',
-        contactNo: '9876543210'
-      });
-      await profile.save();
-    }
-
     process.exit(0);
   } catch (error) {
     console.error('Seed Error:', error);
